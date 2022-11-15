6b924fac8e186d93ac1cf1780ef5e4b2339d80a2
blue <1355589128@qq.com>
2022-11-15 18:38:01 +0800

    login

--- app.js
+++ app.js
@@ -5,22 +5,14 @@ import {  reqLogin} from "./services/login/index";
 App({
   globalData:{    
     token:'',    
-
 } ,
 
   onLaunch: function () {
-    this.handleLogin()
+    // this.handleLogin()
   },
   onShow: function () {
     updateManager();
   },
 
-   async handleLogin(){
-     const res=  await reqLogin({ "username": "admin", "password": "admin"})
-     if (res.code===200) {
-      wx.setStorageSync('token',res.token)
-
-     }
-   }
 
 });
